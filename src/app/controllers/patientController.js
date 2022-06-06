954ab0fc19722165e0f54dfd48e62aa959a6a56a
duongdaydungso <vietnamminer2002@gmail.com>
2022-06-06 08:32:43 +0700

    Small change API

--- src/app/controllers/patientController.js
+++ src/app/controllers/patientController.js
@@ -5,65 +5,66 @@ addFormats(ajv);
 const patientQueries = require("../DB/patientQueries");
 
 class PatientsController {
+  // [GET] /all
+  async all(req, res) {
+    const results = await patientQueries.getAllPatients();
+    res.json({
+      error: 0,
+      msg: "All patients",
+      data: results,
+    });
+  }
 
-    // [GET] /all
-    async all(req, res) {
-        const results = await patientQueries.getAllPatients();
-        res.json({
-            error: 0,
-            msg: "All patients",
-            data: results
-        })
+  // // [GET] /:SSN
+  // async getBySSN(req, res) {
+  //     const results = await patientQueries.getPatientBySSN(req.param('SSN'));
+  //     res.json({
+  //         error: 0,
+  //         error_msg: "Vaccination by index",
+  //         data: results
+  //     })
+  // }
+
+  // [DELETE] /delete?vaccinationid&patientSocialSecurityNumber
+
+  async deleteFromRegistration(req, res) {
+    const deleteFromRegistrationSchema = {
+      type: "object",
+      properties: {
+        vaccinationid: {
+          type: "string",
+        },
+        SSN: {
+          type: "string",
+        },
+      },
+      required: ["vaccinationid", "SSN"],
+    };
+
+    const valid = ajv.validate(deleteFromRegistrationSchema, req.query);
+    if (!valid) {
+      res.json({
+        error: 10051,
+        error_type: "Invalid delete patient from registration body",
+        data: ajv.errors,
+      });
+      return;
     }
 
-    // // [GET] /:SSN
-    // async getBySSN(req, res) {
-    //     const results = await patientQueries.getPatientBySSN(req.param('SSN'));
-    //     res.json({
-    //         error: 0,
-    //         error_msg: "Vaccination by index",
-    //         data: results
-    //     })
-    // }
-
-    // [DELETE] /delete?vaccinationid&patientSocialSecurityNumber
-
-    async deleteFromRegistration(req, res) {
-
-        const deleteFromRegistrationSchema = {
-            type: "object",
-            properties: {
-                vaccinationid : {
-                    type: "string",
-                },
-                patientSocialSecurityNumber : {
-                    type: "string",
-                }
-            },
-            required: ["vaccinationid", "patientSocialSecurityNumber"]
-        }
-
-        const valid = ajv.validate(deleteFromRegistrationSchema, req.query);
-        if (!valid) {
-            res.json({
-                error: 10051,
-                error_type: "Invalid delete patient from registration body",
-                data: ajv.errors
-            })
-            return;
-        }
-
-        try {
-            const results = await patientQueries.deleteFromRegistrations(req.query.vaccinationid, req.query.patientSocialSecurityNumber);
-            res.json({
-                error: 0,
-                msg: "Delete registration successfully",
-                data: results
-            })
-        } catch (error) {
-            res.json(error)
-        }  
+    try {
+      const results = await patientQueries.deleteFromRegistrations(
+        req.query.vaccinationid,
+        req.query.SSN
+      );
+      res.json({
+        error: 0,
+        msg: "Delete registration successfully",
+        data: results,
+      });
+    } catch (error) {
+      res.json(error);
     }
+  }
 }
 
-module.exports = new PatientsController;
+module.exports = new PatientsController();
--- src/routes/patient.js
+++ src/routes/patient.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const {isAuth, isAdmin, isThisUser} = require('../app/auth/authMiddlewares');
+const { isAuth, isAdmin, isThisUser } = require("../app/auth/authMiddlewares");
 
-const patientController = require('../app/controllers/patientController');
+const patientController = require("../app/controllers/patientController");
 
-route.get('/all', isAuth, isAdmin, patientController.all);
-route.delete('/delete', isAuth, isAdmin, patientController.deleteFromRegistration);
+route.get("/all", isAuth, isAdmin, patientController.all);
+route.delete(
+  "/delete",
+  isAuth,
+  isThisUser,
+  patientController.deleteFromRegistration
+);
 
 module.exports = route;
